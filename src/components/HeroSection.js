eee13814a329eff785f60cf05e9f7888aacf6e39
Shubham Jaiswal <sj805217@gmail.com>
2023-02-16 18:12:40 +0530

    Day 2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -24,10 +24,37 @@ export default function BasicCard() {
             level="h6"
             fontWeight="lg"
             textColor="#fff"
-            mt={{ xs: 12, sm: 30 }}
+            mt={{ xs: 12, sm: 45 }}
           >
-            <form>
-                <input type="text" name="" id="" placeholder='From'/>
+              <Typography
+            level="h6"
+            fontWeight="lg"
+            textColor="#fff"
+            mt={{ xs: 12, sm: 30 }}
+          ><h1 id='heroText'>Let the journey begin</h1></Typography>
+
+            <form id="hero_search_flights">
+                <div className='inputConatiner'>
+                <label htmlFor="from">From</label>
+                <input type="text" name="" id="from" placeholder='Amsterdam'/>
+                </div>
+
+                <div className='inputConatiner'>
+                <label htmlFor="to">To</label>
+                <input type="text" name="" id="to" placeholder='Stockholm'/>
+                </div>
+
+                <div className='inputConatiner'>
+                <label htmlFor="depart">Depart</label>
+                <input type="date" name="" id="depart" placeholder='01/Sa/2023'/>
+                </div>
+
+                <div className='inputConatiner'>
+                <label htmlFor="return">Return</label>
+                <input type="date" name="" id="return" placeholder='01/Sa/2023'/>
+                </div>
+
+                <button type="submit" id="btn">SEARCH FLIGHTS</button>
             </form>
           </Typography>
         </CardContent>
